d19767ba15e5f56445f9ef67e515b5d325584bf0
= <nellowly>
2021-12-03 23:28:32 +0700

    3000

--- lua/cp.lua
+++ lua/cp.lua
@@ -387,38 +387,6 @@ function process(data)
   end
 end
 
-local uv = vim.loop
-
-function receive()
-  local buffer = ""
-  server = uv.new_tcp()
-  server:bind("127.0.0.1", 27121)
-  server:listen(128, function(err)
-    assert(not err, err)
-    local client = uv.new_tcp()
-    server:accept(client)
-    client:read_start(function(error, chunk)
-      assert(not error, error)
-      if chunk then
-        buffer = buffer .. chunk
-      else
-        client:shutdown()
-        client:close()
-        local lines = {}
-        for line in string.gmatch(buffer, "[^\r\n]+") do
-          table.insert(lines, line)
-        end
-        buffer = lines[#lines]
-        vim.schedule(function()
-          process(buffer)
-        end)
-        server:shutdown()
-      end
-    end)
-  end)
-  uv.run()
-end
-
 function save()
   for name, s in pairs(P) do
     local path = io.open(string.format("%s/%s.json", s.problemPath, s.name), "w")
@@ -427,8 +395,6 @@ function save()
 end
 
 function start()
-  vim.o.hidden = true
-  vim.o.termguicolors = true
   C:keymaps()
 
 --// lua syntax soon //--
@@ -451,7 +417,7 @@ function CpTest(num, clicks, button, flags)
 endfunction]]
 --\\ lua syntax soon \\--
 
-  receive()
+  require'receive'
 end
 
 function setup(user_config)
